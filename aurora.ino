e83a49b0fa7d7f8cf5a933c377f3dc2d1e4526fe
null-route <tyler@bacon.io>
2017-11-09 00:10:27 -0800

    Refactoring; blooms are now a class

--- aurora.ino
+++ aurora.ino
@@ -3,6 +3,7 @@
 #define PIN 6
 #define TOTALPIXELS 120
 
+// Initialize the strip object
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(TOTALPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 // Initialize an array of pixel structs
@@ -16,8 +17,48 @@ void setup() {
 
 // Begin main loop
 void loop() {
-  breath();
+  aurora();
 }
+
+void aurora() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void breath() {
   // Set random pixel color values
@@ -26,20 +67,20 @@ void breath() {
   tarColor.g = 0;
   tarColor.b = random(1,255);
   // Set target pixel location within the strip
-  uint16_t targetPixelLocation = random(1, TOTALPIXELS);
+  uint16_t center = random(1, TOTALPIXELS);
   // Set target range (breadth of light spread)
   uint16_t range = random(TOTALPIXELS / 10, TOTALPIXELS / 2);
   // Start inhaling
-  inhale(targetPixelLocation, range, tarColor);
+  inhale(center, range, tarColor);
   // Start exhaling
-  exhale(targetPixelLocation, range, tarColor);
+  exhale(center, range, tarColor);
 }
 
 // Function for creating an 'inhale' animation.
 // Color increases in intensity and breadth as a function of time.
-void inhale (uint16_t targetPixelLocation, uint16_t range, PixelStruct tarColor) {
+void inhale (uint16_t center, uint16_t range, PixelStruct tarColor) {
   // Set target inhale duration
-  uint16_t inhaleDuration = random(10,100);
+  double inhaleDuration = random(10,100);
   // Set a marker to signal we're inhaling
   bool marker = true;
   // Begin inhaling for the defined duration
@@ -51,15 +92,15 @@ void inhale (uint16_t targetPixelLocation, uint16_t range, PixelStruct tarColor)
       for(uint8_t b=0; b<2; b++) {
         // If we're at the first step (the center), just use that location
         if(x == 0) {
-          mutatePixel(targetPixelLocation, x, range, y, inhaleDuration, tarColor, marker);
+          mutatePixel(center, x, range, y, inhaleDuration, tarColor, marker);
         }
         // Selects the left pixel of the current range
         else if(b == 0) {
-          mutatePixel((targetPixelLocation - x), x, range, y, inhaleDuration, tarColor, marker);
+          mutatePixel((center - x), x, range, y, inhaleDuration, tarColor, marker);
         }
         // Selects the right pixel of the current range
         else if(b == 1) {
-          mutatePixel((targetPixelLocation + x), x, range, y, inhaleDuration, tarColor, marker);
+          mutatePixel((center + x), x, range, y, inhaleDuration, tarColor, marker);
         }
       }
     }
@@ -108,7 +149,7 @@ uint16_t curDur, uint16_t tarDur, PixelStruct tarColor, bool marker) {
   // Check to make sure that our target is in a valid range
   if(location > 0 && location < TOTALPIXELS) {
     // Calculate modified color values based on range and duration.
-    // This math may be fucked up. idklol I'm terrible at math.
+    // This needs to be optimized.
     double intensity = ((double(curDur) / double(tarDur)) * (.9999 - double(curRange) / double(tarRange)));
     uint32_t calcR = (tarColor.r * intensity);
     uint32_t calcG = (tarColor.g * intensity);
--- pixelstruct.h
+++ pixelstruct.h
@@ -7,6 +7,93 @@ struct PixelStruct {
   uint8_t r = 1;
   uint8_t g = 1;
   uint8_t b = 1;
+  // Tell us if this pixel is blooming currently
+  // This allows us to restrict writes, allowing correct behavior
+  bool blooming = false;
 };
+
+// Create a struct for creating blooms
+struct BloomStruct {
+  // Define where the center of the bloom is located
+  uint16_t center = 0;
+  // Desired range/breadth of bloom
+  double range = 0;
+  // Define target color values for the bloom
+  uint8_t r = 0;
+  uint8_t g = 0;
+  uint8_t b = 0;
+  // Bloom grow state and target
+  double cGrow = 0;
+  double tGrow = 0;
+  // Bloom shrink state and target
+  double cShrink = 0;
+  double tShrink = 0;
+  // Bloom intensity modifier
+  double intensity = 0;
+
+
+  // Initialize a new randomized bloom
+  void initialize() {
+    center = random(0, TOTALPIXELS);
+    range = random(TOTALPIXELS / 10, TOTALPIXELS / 2);
+    r = random(0,255);
+    g = random(0,255);
+    b = random(0,255);
+    tGrow = random(10,100);
+    cGrow = 0;
+    tShrink = random(10,100);
+    cShrink = tShrink;
+  }
+  // Advance one step through the animation
+  void grow() {
+    if(cGrow != tGrow) { // Begin growing the bloom to the target
+      intensity = cGrow / tGrow;
+      cGrow++;
+    }
+    else if(cShrink != 0) { // Begin shrinking the bloom to 0
+      intensity = cShrink / tShrink;
+      --cShrink;
+    }
+    else if(cGrow == tGrow && cShrink == 0) { // If bloom has completed, reinitialize
+      initialize();
+    }
+  }
+  // Apply the current bloom state to the pixel array
+  void apply(PixelStruct arr[]) {
+
+
+    double intensity = ((double(curDur) / double(tarDur)) * (.9999 - double(curRange) / double(tarRange)));
+
+    for(uint16_t x=0; x<range; x++) {
+      // Alternate between positive and negative values of the range
+      // This could probably be more elegant
+      arr[center - x]
+      arr[center + x]
+
+
+
+
+
+
+
+
+      for(uint8_t b=0; b<2; b++) {
+        // If we're at the first step (the center), just use that location
+        if(x == 0) {
+          arr[center]
+          mutatePixel(center, x, range, y, exhaleDuration, tarColor, marker);
+        }
+        // Selects the left pixel of the current range
+        else if(b == 0) {
+          mutatePixel((targetPixelLocation - x), x, range, y, exhaleDuration, tarColor, marker);
+        }
+        // Selects the right pixel of the current range
+        else if(b == 1) {
+          mutatePixel((targetPixelLocation + x), x, range, y, exhaleDuration, tarColor, marker);
+        }
+      }
+    }
+  }
+}
 
 #endif
