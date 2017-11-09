62674ce185af5fedcfdb016807b94c55103717b8
null-route <tyler@bacon.io>
2017-11-08 19:11:05 -0800

    Initial commit

---
+++ pixelstruct.h
@@ -0,0 +1,12 @@
+#ifndef pixelstruct_h
+#define pixelstruct_h
+#include "Arduino.h"
+
+// Create a struct for holding pixel color values
+struct PixelStruct {
+  uint8_t r = 1;
+  uint8_t g = 1;
+  uint8_t b = 1;
+};
+
+#endif
