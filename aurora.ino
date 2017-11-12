fb2227cc9145c9e232f7c9e9361cc429bb8cefc5
null-route <tyler@bacon.io>
2017-11-11 19:47:32 -0800

    Working version, need to fix brightness

--- aurora.ino
+++ aurora.ino
@@ -1,13 +1,15 @@
 #include "Adafruit_NeoPixel.h"
 #include "pixelstruct.h"
 #define PIN 6
-#define TOTALPIXELS 120
+#define TOTALPIXELS 450
+#define TOTALBLOOMS 10
 
 // Initialize the strip object
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(TOTALPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-// Initialize an array of pixel structs
-PixelStruct pixel[TOTALPIXELS];
+// Create an array of pixel structs and blooms
+uint8_t pixels[TOTALPIXELS][4] = {0};
+BloomStruct blooms[TOTALBLOOMS];
 
 // Initial setup
 void setup() {
@@ -17,169 +19,29 @@ void setup() {
 
 // Begin main loop
 void loop() {
+  // Initialize all blooms
+  for(int8_t x=0; x<TOTALBLOOMS; x++) {
+    blooms[x].initialize(TOTALPIXELS);
+  }
+  // Begin the animation
   aurora();
 }
 
+// Function to generate multiple blooms of color
 void aurora() {
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void breath() {
-  // Set random pixel color values
-  struct PixelStruct tarColor;
-  tarColor.r = random(1,255);
-  tarColor.g = 0;
-  tarColor.b = random(1,255);
-  // Set target pixel location within the strip
-  uint16_t center = random(1, TOTALPIXELS);
-  // Set target range (breadth of light spread)
-  uint16_t range = random(TOTALPIXELS / 10, TOTALPIXELS / 2);
-  // Start inhaling
-  inhale(center, range, tarColor);
-  // Start exhaling
-  exhale(center, range, tarColor);
-}
-
-// Function for creating an 'inhale' animation.
-// Color increases in intensity and breadth as a function of time.
-void inhale (uint16_t center, uint16_t range, PixelStruct tarColor) {
-  // Set target inhale duration
-  double inhaleDuration = random(10,100);
-  // Set a marker to signal we're inhaling
-  bool marker = true;
-  // Begin inhaling for the defined duration
-  for(uint16_t y=1; y<inhaleDuration; y++) {
-    // Iterate through each step in the range
-    for(uint16_t x=0; x<range; x++) {
-      // Alternate between positive and negative values of the range
-      // This could probably be more elegant
-      for(uint8_t b=0; b<2; b++) {
-        // If we're at the first step (the center), just use that location
-        if(x == 0) {
-          mutatePixel(center, x, range, y, inhaleDuration, tarColor, marker);
-        }
-        // Selects the left pixel of the current range
-        else if(b == 0) {
-          mutatePixel((center - x), x, range, y, inhaleDuration, tarColor, marker);
-        }
-        // Selects the right pixel of the current range
-        else if(b == 1) {
-          mutatePixel((center + x), x, range, y, inhaleDuration, tarColor, marker);
-        }
-      }
-    }
-    // Update the strip after every change to the given range
-    strip.show();
+  while(1) {
+  // Iterate through the number of blooms
+  for(uint8_t x=0; x<TOTALBLOOMS; x++) {
+    // Progress the bloom one step
+    blooms[x].run();
+    // Apply the bloom to the pixel array
+    blooms[x].apply(pixels);
   }
-}
-
-// Function for creating an 'exhale' animation.
-// Color decreases in intensity and breadth as a function of time.
-void exhale (uint16_t targetPixelLocation, uint16_t range, PixelStruct tarColor) {
-  // Set target exhale duration
-  uint16_t exhaleDuration = random(10,100);
-  // Set a marker to signal we're exhaling
-  bool marker = false;
-  // Begin exhaling for the defined duration
-  for(uint16_t y=exhaleDuration; y>0; --y) {
-    // Iterate through each step in the range
-    for(uint16_t x=0; x<range; x++) {
-      // Alternate between positive and negative values of the range
-      // This could probably be more elegant
-      for(uint8_t b=0; b<2; b++) {
-        // If we're at the first step (the center), just use that location
-        if(x == 0) {
-          mutatePixel(targetPixelLocation, x, range, y, exhaleDuration, tarColor, marker);
-        }
-        // Selects the left pixel of the current range
-        else if(b == 0) {
-          mutatePixel((targetPixelLocation - x), x, range, y, exhaleDuration, tarColor, marker);
-        }
-        // Selects the right pixel of the current range
-        else if(b == 1) {
-          mutatePixel((targetPixelLocation + x), x, range, y, exhaleDuration, tarColor, marker);
-        }
-      }
-    }
-    // Update the strip after every change to the given range
-    strip.show();
+  // Apply our pixel array changes to the strip
+  for(uint16_t y=0; y<TOTALPIXELS; y++) {
+    strip.setPixelColor(y, pixels[y][1], pixels[y][2], pixels[y][3]);
   }
-}
-
-// Function for mutating pixel colors based on duration, range, and target
-// color values versus those currently present.
-void mutatePixel(uint16_t location, uint16_t curRange, uint16_t tarRange,
-uint16_t curDur, uint16_t tarDur, PixelStruct tarColor, bool marker) {
-  // Check to make sure that our target is in a valid range
-  if(location > 0 && location < TOTALPIXELS) {
-    // Calculate modified color values based on range and duration.
-    // This needs to be optimized.
-    double intensity = ((double(curDur) / double(tarDur)) * (.9999 - double(curRange) / double(tarRange)));
-    uint32_t calcR = (tarColor.r * intensity);
-    uint32_t calcG = (tarColor.g * intensity);
-    uint32_t calcB = (tarColor.b * intensity);
-    // Compare calculated values to current values and replace if greater.
-    // Unsure if this logic will produce the desired effect.
-    // Actual testing will be required.
-    if(marker) {
-      if(calcR > pixel[location].r) {
-        pixel[location].r = calcR;
-      }
-      if(calcG > pixel[location].g) {
-        pixel[location].g = calcG;
-      }
-      if(calcB > pixel[location].b) {
-        pixel[location].b = calcB;
-      }
-    }
-    else {
-      if(calcR < pixel[location].r) {
-        pixel[location].r = calcR;
-      }
-      if(calcG < pixel[location].g) {
-        pixel[location].g = calcG;
-      }
-      if(calcB < pixel[location].b) {
-        pixel[location].b = calcB;
-      }
-    }
-    // Set the strip's pixel color to the values of the current pixel.
-    strip.setPixelColor(location, pixel[location].r, pixel[location].g, pixel[location].b);
+  // Write all changes to the bus
+  strip.show();
   }
 }
--- pixelstruct.h
+++ pixelstruct.h
@@ -2,26 +2,14 @@
 #define pixelstruct_h
 #include "Arduino.h"
 
-// Create a struct for holding pixel color values
-struct PixelStruct {
-  uint8_t r = 1;
-  uint8_t g = 1;
-  uint8_t b = 1;
-  // Tell us if this pixel is blooming currently
-  // This allows us to restrict writes, allowing correct behavior
-  bool blooming = false;
-};
-
 // Create a struct for creating blooms
 struct BloomStruct {
   // Define where the center of the bloom is located
-  uint16_t center = 0;
+  int16_t center = 0;
   // Desired range/breadth of bloom
   double range = 0;
   // Define target color values for the bloom
-  uint8_t r = 0;
-  uint8_t g = 0;
-  uint8_t b = 0;
+  double color[3] = {0, 0, 0};
   // Bloom grow state and target
   double cGrow = 0;
   double tGrow = 0;
@@ -30,70 +18,59 @@ struct BloomStruct {
   double tShrink = 0;
   // Bloom intensity modifier
   double intensity = 0;
-
+  // Keep track of total pixels internally
+  uint16_t localPixels = 0;
 
   // Initialize a new randomized bloom
-  void initialize() {
-    center = random(0, TOTALPIXELS);
-    range = random(TOTALPIXELS / 10, TOTALPIXELS / 2);
-    r = random(0,255);
-    g = random(0,255);
-    b = random(0,255);
-    tGrow = random(10,100);
+  void initialize(uint16_t totalPixels) {
+    localPixels = totalPixels;
+    center = random(0, localPixels);
+    range = random(localPixels / 20, localPixels / 10);
+    color[1] = random(0,200);
+    color[2] = 0;
+    color[3] = random(100,255);
+    tGrow = random(10,50);
     cGrow = 0;
-    tShrink = random(10,100);
+    tShrink = random(10,50);
     cShrink = tShrink;
   }
-  // Advance one step through the animation
-  void grow() {
-    if(cGrow != tGrow) { // Begin growing the bloom to the target
+
+  // Grow the bloom one step
+  void run() {
+    if(cGrow < tGrow) { // Begin growing the bloom to the target
       intensity = cGrow / tGrow;
       cGrow++;
     }
-    else if(cShrink != 0) { // Begin shrinking the bloom to 0
+    else if(cShrink > 0) { // Begin shrinking the bloom to 0
       intensity = cShrink / tShrink;
-      --cShrink;
+      cShrink--;
     }
-    else if(cGrow == tGrow && cShrink == 0) { // If bloom has completed, reinitialize
-      initialize();
+    else if(cShrink <= 0) { // If bloom has completed, reinitialize
+      initialize(localPixels);
     }
   }
+
   // Apply the current bloom state to the pixel array
-  void apply(PixelStruct arr[]) {
-
-
-    double intensity = ((double(curDur) / double(tarDur)) * (.9999 - double(curRange) / double(tarRange)));
-
-    for(uint16_t x=0; x<range; x++) {
-      // Alternate between positive and negative values of the range
-      // This could probably be more elegant
-      arr[center - x]
-      arr[center + x]
-
-
-
-
-
-
-
-
-      for(uint8_t b=0; b<2; b++) {
-        // If we're at the first step (the center), just use that location
-        if(x == 0) {
-          arr[center]
-          mutatePixel(center, x, range, y, exhaleDuration, tarColor, marker);
+  // PREPARE FOR CRAZY NESTED FOR LOOPS 0_____0
+  void apply(uint8_t pixels[][4]) {
+    for(uint8_t x=0; x<range; x++) {
+      // Calculate the color modifier for the pixel (time*range offset)
+      double colorModifier = (intensity * (.9999 - (double(x) / range)));
+      // Alternate between positive and negative offset values
+      // (Except for when the range is 0 -- when we're in the center)
+      for(uint8_t rgb=1; rgb<4; rgb++) {
+        // If the color value is 0, set to target color value (no averaging)
+        // Also wrapped in if statements to protect from addressing outside
+        // the array.
+        if((center + x) < localPixels) {
+          pixels[center + x][rgb] = ((pixels[center + x][rgb] + (color[rgb] * colorModifier)) / 2);
         }
-        // Selects the left pixel of the current range
-        else if(b == 0) {
-          mutatePixel((targetPixelLocation - x), x, range, y, exhaleDuration, tarColor, marker);
-        }
-        // Selects the right pixel of the current range
-        else if(b == 1) {
-          mutatePixel((targetPixelLocation + x), x, range, y, exhaleDuration, tarColor, marker);
+        if((center - x) > 0) {
+          pixels[center - x][rgb] = ((pixels[center - x][rgb] + (color[rgb] * colorModifier)) / 2);
         }
       }
     }
   }
-}
+};
 
 #endif
